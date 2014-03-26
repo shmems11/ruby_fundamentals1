af8370caf45ad2934414cd5538602c72ed9cba72
shmems11 <shmems11@gmail.com>
2014-03-26 09:55:54 -0400

    exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+tip = 55 * 0.2
+puts tip
+#good tip for a 55 dollar meal
+
+puts "5".to_i + 4
+#adding a string and an integer
+
+num = 45628 * 7839
+puts "The answer is #{num}."
+#string interpolation
+
+(true && false) || (false && true) || !(false && false)
+#evaluates true

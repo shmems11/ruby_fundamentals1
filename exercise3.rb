aa893026013f0548c43d4f21ba2ed98cdd79a4a9
shmems11 <shmems11@gmail.com>
2014-03-26 13:08:35 -0400

    birth year

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,10 @@
 puts "What's your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+puts "How old are you?"
+age = gets.chomp
+present_year = 2014
+birth_year = present_year - age.to_i
+
+puts "You were born in the year #{birth_year}."
